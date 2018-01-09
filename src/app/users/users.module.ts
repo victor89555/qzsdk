47bf5999b663464a612e3547abc69d3a71004709
zhaixm <zxmsdyz@sina.com>
2018-01-09 10:26:48 +0800

    修改商户订单流水tab链接

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,15 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { PersonalCenterComponent } from './personal-center/personal-center.component';
-import { OrderListComponent } from './order-list/order-list.component';
-import { TabListComponent } from './tab-list/tab-list.component';
-import { IntegralListComponent } from './integral-list/integral-list.component';
-import { UsersComponent } from './users.component';
-import { routes } from './users.routes';
-import { RouterModule } from "@angular/router";
-import { OrderDetailComponent } from './order-detail/order-detail.component';
+import {PersonalCenterComponent} from './personal-center/personal-center.component';
+import {OrderListComponent} from './order-list/order-list.component';
+import {TabListComponent} from './tab-list/tab-list.component';
+import {IntegralListComponent} from './integral-list/integral-list.component';
+import {UsersComponent} from './users.component';
+import {routes} from './users.routes';
+import {RouterModule} from "@angular/router";
+import {OrderDetailComponent} from './order-detail/order-detail.component';
+import {LoginComponent} from "./login/login.component"
 
 @NgModule({
   imports: [
@@ -23,7 +24,9 @@ import { OrderDetailComponent } from './order-detail/order-detail.component';
     OrderListComponent,
     TabListComponent,
     IntegralListComponent,
-    OrderDetailComponent
+    OrderDetailComponent,
+    LoginComponent
   ]
 })
-export class UsersModule {}
+export class UsersModule {
+}
