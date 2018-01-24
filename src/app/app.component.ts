1cf2b435e132f1bd87d4912fcb143f0c363a68bd
Victor <573864600@qq.com>
2018-01-24 16:05:59 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,13 +85,13 @@ export class AppComponent {
           console.log(res.error.msg)
           const errCode = res.error.errCode
           if (errCode == "NO_FOLLOW_MP") {
-            //todo 转到公众号关注页面
+            //转到公众号关注页面
             this.router.navigateByUrl("/user/follow")
           } else if (errCode == "NOT_MEMBER") {
-            //todo 转到会员注册页面
+            //转到会员注册页面
             this.router.navigateByUrl("/user/register")
           } else if (errCode == "NOT_OPERATOR") {
-            //todo 转到商户绑定页面
+            //转到商户绑定页面
             this.router.navigateByUrl("/shop/bind")
           }
 
